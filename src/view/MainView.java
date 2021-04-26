6fd6b43d98971ed6301d4711d8810578717f4b32
Alladin-51 <igortls20@gmail.com>
2021-04-26 18:23:13 -0300

    Método main adicionado

---
+++ src/view/MainView.java
@@ -0,0 +1,14 @@
+package view;
+
+// Método main 
+
+public class MainView {
+
+	public static void main(String[] args) {
+		
+		// Chamada da tela principal
+		new TelaPrincipal().setVisible(true);
+	}
+
+}
+ 

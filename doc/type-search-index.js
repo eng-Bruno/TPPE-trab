55bc5a59d7b803754aac0c656f28752d37a75209
Alladin-51 <igortls20@gmail.com>
2021-04-28 12:21:53 -0300

    JavaDoc Velocidade

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"converter","l":"Tempo"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"converter","l":"Velocidade"}];updateSearchResults();

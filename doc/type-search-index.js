fba108369c6f6347ea0d61660197428b841ecaf7
Alladin-51 <igortls20@gmail.com>
2021-04-28 12:22:24 -0300

    JavaDoc Volume

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"converter","l":"Velocidade"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"converter","l":"Volume"}];updateSearchResults();

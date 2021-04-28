494919938b038c34ea43f645313068799d832895
Alladin-51 <igortls20@gmail.com>
2021-04-28 12:20:01 -0300

    JavaDoc Dados

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"converter","l":"ConverteMedida"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"converter","l":"Dados"}];updateSearchResults();

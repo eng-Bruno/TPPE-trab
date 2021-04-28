5f310c1751e04b751e0b0c394818094fa3146b39
Alladin-51 <igortls20@gmail.com>
2021-04-28 12:07:26 -0300

    Update JavaDoc Classe abstrata

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"converter","l":"Comprimento"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"converter","l":"ConverteMedida"}];updateSearchResults();

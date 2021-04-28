844a983e7b510725eb8296868491d0bc62849a46
Alladin-51 <igortls20@gmail.com>
2021-04-28 12:20:28 -0300

    JavaDoc Peso

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"converter","l":"Dados"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"converter","l":"Peso"}];updateSearchResults();

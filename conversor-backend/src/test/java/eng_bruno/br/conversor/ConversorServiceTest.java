a9445e940fe6e58c801d71d5ad0844c54dad306e
Bruno Oliveira <brunooliveira.unb@gmail.com>
2025-06-03 23:39:07 -0300

    feat(tests): create tdd (skipped)

---
+++ conversor-backend/src/test/java/eng_bruno/br/conversor/ConversorServiceTest.java
@@ -0,0 +1,17 @@
+package eng_bruno.br.conversor;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled("TDD")
+public class ConversorServiceTest {
+
+    @Test
+    void testeIgnorado() {
+        assertTrue(true);
+    }
+}
